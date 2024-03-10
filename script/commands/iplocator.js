7c17cc183ece66d9647b93bbab53d54ec10bf2a9
Harold Hutchins <160975590+haroldhutchin2005@users.noreply.github.com>
2024-03-10 17:52:46 +0800

    Create iplocator.js

---
+++ script/commands/iplocator.js
@@ -0,0 +1,55 @@
+const axios = require('axios');
+
+module.exports.config = {
+    name: "iplocator",
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "Jonell Magallanes",
+    description: "IP Locator",
+    usePrefix: false,
+    commandCategory: "other",
+    usages: "[ip]",
+    cooldowns: 10
+};
+
+module.exports.run = async function ({ api, event, args }) {
+    const content = encodeURIComponent(args.join(" "));
+    
+    if (!content) return api.sendMessage("Please provide a valid IP address.\n\nExample: iplocator 111.90.225.96", event.threadID, event.messageID);
+
+    const apiUrl = `https://jonellccapisproject-e1a0d0d91186.herokuapp.com/api/ip?ipnum=${content}`;
+
+    try {
+        api.sendMessage("🔍 | Locating IP address. Please wait...", event.threadID, event.messageID);
+
+        const response = await axios.get(apiUrl);
+        const { data } = response.data;
+
+        if (data && data.success) {
+            const {
+                ip, type, continent, country, region, city, latitude, longitude,
+                org, isp, timezone, currency, currency_symbol
+            } = data;
+
+            const replyMessage = `ℹ️ IP Information:\n
+📫 IP: ${ip}
+🛰️ Type: ${type}
+🗾 Continent: ${continent}
+🌏 Country: ${country}
+🗺️ Region: ${region}
+📍 City: ${city}
+🔍 Coordinates: ${latitude}, ${longitude}
+🛰️ Organization: ${org}
+📝 ISP: ${isp}
+⏰ Timezone: ${timezone}
+💵 Currency: ${currency} (${currency_symbol})`;
+
+            api.sendMessage(replyMessage, event.threadID, event.messageID);
+        } else {
+            api.sendMessage("Invalid IP address or unable to fetch information.", event.threadID);
+        }
+    } catch (error) {
+        console.error(error);
+        api.sendMessage("An error occurred while processing your request.", event.threadID);
+    }
+};
