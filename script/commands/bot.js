ecc522f62391d5b48e2d0cff4d91ee8bf6b1e875
Harold Hutchins <160975590+haroldhutchin2005@users.noreply.github.com>
2024-03-24 18:28:42 +0800

    Update bot.js

--- script/commands/bot.js
+++ script/commands/bot.js
@@ -4,8 +4,8 @@ module.exports.config = {
   name: "bot",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "Jonell Magallanes", // Credits for the API KENLIEPLAYS
-  description: "chat with bot",//powered by simini command
+  credits: "Jonell Magallanes",
+  description: "chat with bot",
   usePrefix: true,
   commandCategory: "chat",
   usages: "bot [your message]",
@@ -13,22 +13,28 @@ module.exports.config = {
 };
 
 module.exports.run = async ({ api, event, args }) => {
-  const message = args.join(" ") || "hi"; api.setMessageReaction("✅", event.messageID, () => { }, true);
-  if (!args[0]) return api.sendMessage("Please input your message", event.threadID, event.messageID); api.setMessageReaction("🗨️", event.messageID, () => { }, true);
+  const message = args.join(" ") || "hi";
+  api.setMessageReaction("✅", event.messageID, () => { }, true);
+  
+  if (!args[0]) {
+    api.sendMessage("Please input your message", event.threadID, event.messageID);
+    api.setMessageReaction("🗨️", event.messageID, () => { }, true);
+    return;
+  }
 
-  const apiUrl = `https://simsimi.fun/api/v2/?mode=talk&lang=en&message=${encodeURIComponent(message)}&filter=true`;
+  const apiUrl = `https://sim-api-by-jonell-magallanes.onrender.com/sim?query=${encodeURIComponent(message)}`;
 
   try {
     const response = await axios.get(apiUrl);
-    if (response.data.success) {
-      api.sendMessage(response.data.success, event.threadID, event.messageID); api.setMessageReaction("❤️", event.messageID, () => { }, true);
+    if (response.data.respond) {
+      api.sendMessage(response.data.respond, event.threadID, event.messageID);
+      api.setMessageReaction("❤️", event.messageID, () => { }, true);
     } else {
       api.sendMessage("Unable to get a response from the bot.", event.threadID, event.messageID);
     }
   } catch (error) {
     console.error('Error making API Call:', error);
-    api.sendMessage('An error occurred while talking to the bot.', event.threadID, event.messageID); api.setMessageReaction("😥", event.messageID, () => { }, true);
-
-
+    api.sendMessage('An error occurred while talking to the bot.', event.threadID, event.messageID);
+    api.setMessageReaction("😥", event.messageID, () => { }, true);
   }
 };
