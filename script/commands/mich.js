95c6aabdf44261052f82684266ccd2c13c9f3b2b
Harold Hutchins <160975590+haroldhutchin2005@users.noreply.github.com>
2024-04-16 18:59:11 +0800

    Update mich.js

--- script/commands/mich.js
+++ script/commands/mich.js
@@ -19,12 +19,12 @@ module.exports.run = async function ({ api, event, args }) {
     
     if (command === 'on') {
         chatEnabled = true;
-        return api.sendMessage("Mich is now ON", event.threadID, event.messageID);
+        return api.sendMessage("Chatbot is now ON", event.threadID, event.messageID);
     }
 
     if (command === 'off') {
         chatEnabled = false;
-        return api.sendMessage("Mich is now OFF", event.threadID, event.messageID);
+        return api.sendMessage("Chatbot is now OFF", event.threadID, event.messageID);
     }
 
     if (!chatEnabled) return; 
@@ -35,16 +35,12 @@ module.exports.run = async function ({ api, event, args }) {
 
     if (!content) return api.sendMessage("Hello There I'm Mich Chatbot made by Jonell Magallanes Haha", event.threadID, event.messageID);
     
-    api.sendTypingIndicator(event.threadID);
-
     apiUrl = `https://michai-ec9807495fa2.herokuapp.com/mich?ask=${content}&id=${id}`;
 
     try {
         const response = await axios.get(apiUrl);
         const { response: reply } = response.data;  
-        api.sendMessage(reply, event.threadID, () => {
-            api.sendTypingIndicator(event.threadID, false); 
-        });
+        api.sendMessage(reply, event.threadID);
     } catch (error) {
         console.error(error);
         api.sendMessage("Nagkaroon ng Error Sa Main Server ng API Please Try Again Later or Contact the Developer Jonell Magallanes Thanks", event.threadID);
@@ -52,26 +48,20 @@ module.exports.run = async function ({ api, event, args }) {
 };
 
 module.exports.handleEvent = async function ({ api, event }) {
-    if (!chatEnabled) return;
+    if (!chatEnabled || event.body === null || !event.isGroup) return;
 
-    if (event.body !== null && event.isGroup) {
-        const content = encodeURIComponent(event.body);
-        const id = event.senderID;  
-        let apiUrl;
+    const content = encodeURIComponent(event.body);
+    const id = event.senderID;  
+    let apiUrl;
 
-        api.sendTypingIndicator(event.threadID);
+    apiUrl = `https://michai-ec9807495fa2.herokuapp.com/mich?ask=${content}&id=${id}`;
 
-        apiUrl = `https://michai-ec9807495fa2.herokuapp.com/mich?ask=${content}&id=${id}`;
-
-        try {
-            const response = await axios.get(apiUrl);
-            const { response: reply } = response.data;  
-            api.sendMessage(reply, event.threadID, () => {
-                api.sendTypingIndicator(event.threadID, false); 
-            });
-        } catch (error) {
-            console.error(error);
-            api.sendMessage("Nagkaroon ng Error Sa Main Server ng API Please Try Again Later or Contact the Developer Jonell Magallanes Thanks", event.threadID);
-        }
+    try {
+        const response = await axios.get(apiUrl);
+        const { response: reply } = response.data;  
+        api.sendMessage(reply, event.threadID);
+    } catch (error) {
+        console.error(error);
+        api.sendMessage("Nagkaroon ng Error Sa Main Server ng API Please Try Again Later or Contact the Developer Jonell Magallanes Thanks", event.threadID);
     }
 };
